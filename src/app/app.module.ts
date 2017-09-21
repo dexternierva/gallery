9fe6d4fa7ef67f2f4761ed434cb1a129d7a88579
Dexter <dexternierva@gmail.com>
2017-09-21 08:46:05 +0800

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AlertModule } from 'ngx-bootstrap/ng2-bootstrap';
+import { RouterModule } from '@angular/router';
 
+import { ImageFilterPipe } from './image-detail/shared/filter.pipe';
+import { ImageService } from './image-detail/shared/image.service';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ImageDetailComponent } from './image-detail/image-detail.component';
+import { appRoutes } from '../routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GalleryComponent,
+    ImageDetailComponent,
+    ImageFilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AlertModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [ImageService, ImageFilterPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
